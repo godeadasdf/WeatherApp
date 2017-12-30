1c227736ef09a8fd6a05181194afa24516b3f4b8
kangweichen <793356239@qq.com>
2017-12-30 16:17:48 +0800

    before weight=1

--- app/src/main/java/com/example/kangning/weatherapp/Main2Activity.java
+++ app/src/main/java/com/example/kangning/weatherapp/Main2Activity.java
@@ -2,16 +2,29 @@ package com.example.kangning.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.kangning.weatherapp.DataProvider.horizontalChart.HorizontalChartDataProvider;
+import com.example.kangning.weatherapp.DataProvider.horizontalChart.HorizontalChartInfo;
+import com.example.kangning.weatherapp.adapter.HorizontalChartAdapterDual;
 
 
 public class Main2Activity extends AppCompatActivity {
+
+    private HorizontalChartInfo horizontalChartInfo = HorizontalChartDataProvider.getData();
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Line line = findViewById(R.id.line);
-        line.setLength(100);
+        recyclerView = findViewById(R.id.data_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(new HorizontalChartAdapterDual(R.layout.listview_item_horizontal_chart_dual,
+                horizontalChartInfo.getList(),
+                horizontalChartInfo.getList().get(0).getNum()));
     }
 }
 
---
+++ app/src/main/java/com/example/kangning/weatherapp/adapter/HorizontalChartAdapterDual.java
@@ -0,0 +1,38 @@
+package com.example.kangning.weatherapp.adapter;
+
+
+import android.widget.LinearLayout;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.kangning.weatherapp.DataProvider.horizontalChart.HorizontalChartItem;
+import com.example.kangning.weatherapp.R;
+
+import java.util.List;
+
+/**
+ * Created by kangning on 2017/12/30.
+ */
+
+public class HorizontalChartAdapterDual extends BaseQuickAdapter<HorizontalChartItem, BaseViewHolder> {
+
+    private float lineMaxLength = 1000f; //todo dp -> px
+    private float maxNum;
+
+    public HorizontalChartAdapterDual(int layoutResId, List data, float maxNum) {
+        super(layoutResId, data);
+        this.maxNum = maxNum;
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, HorizontalChartItem item) {
+        helper.setText(R.id.number, item.getNum() + "");
+        helper.setText(R.id.rate, item.getRate());
+        helper.setText(R.id.station, item.getZone_name());
+        LinearLayout lineNum = helper.getView(R.id.number_line);
+        float numLength = (item.getNum() / maxNum) * lineMaxLength;
+        lineNum.setMinimumWidth((int) numLength);
+        LinearLayout lineRate = helper.getView(R.id.rate_line);
+        lineRate.setMinimumWidth((int) ((Float.parseFloat(item.getRate()) / 100f) * numLength));
+    }
+}
