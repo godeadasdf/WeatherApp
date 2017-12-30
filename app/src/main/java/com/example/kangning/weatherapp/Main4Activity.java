d854c5a4b660d9571a61e6a621b49fb4ed1f0bc2
kangweichen <793356239@qq.com>
2017-12-30 20:09:36 +0800

    单行轴完成

---
+++ app/src/main/java/com/example/kangning/weatherapp/Main4Activity.java
@@ -0,0 +1,59 @@
+package com.example.kangning.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.kangning.weatherapp.DataProvider.horizontalChart.HorizontalChartDataProvider;
+import com.example.kangning.weatherapp.DataProvider.horizontalChart.HorizontalChartInfo;
+import com.example.kangning.weatherapp.adapter.HorizontalChartAdapterSingle;
+
+
+public class Main4Activity extends AppCompatActivity {
+
+    private HorizontalChartInfo horizontalChartInfo = HorizontalChartDataProvider.getData();
+    private RecyclerView recyclerView;
+    private LinearLayout forMeasure;
+    private LinearLayout measureLine;
+    private TextView measureNum;
+
+    private int maxLength; //条最大长度
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        //求解最大长度
+
+        // forMeasure.setVisibility(View.GONE);
+
+        //显示list
+        recyclerView = findViewById(R.id.data_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        forMeasure = findViewById(R.id.for_measure);
+        measureLine = findViewById(R.id.measure_line);
+        measureNum = findViewById(R.id.measure_num);
+        measureNum.setText(horizontalChartInfo.getList().get(0).getNum() + "");
+        //todo why need -100
+        maxLength = measureLine.getWidth() - 100;
+        forMeasure.setVisibility(View.GONE);
+        recyclerView.setAdapter(new HorizontalChartAdapterSingle(R.layout.listview_item_horizontal_chart_single,
+                horizontalChartInfo.getList(),
+                horizontalChartInfo.getList().get(0).getNum(),
+                (float) maxLength));
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+}
+
--- app/src/main/java/com/example/kangning/weatherapp/adapter/HorizontalChartAdapterSingle.java
+++ app/src/main/java/com/example/kangning/weatherapp/adapter/HorizontalChartAdapterSingle.java
@@ -1,6 +1,5 @@
 package com.example.kangning.weatherapp.adapter;
 
-
 import android.widget.LinearLayout;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -14,12 +13,11 @@ import java.util.List;
  * Created by kangning on 2017/12/30.
  */
 
-public class HorizontalChartAdapterDual extends BaseQuickAdapter<HorizontalChartItem, BaseViewHolder> {
-
+public class HorizontalChartAdapterSingle extends BaseQuickAdapter<HorizontalChartItem, BaseViewHolder> {
     private float lineMaxLength;
     private float maxNum;
 
-    public HorizontalChartAdapterDual(int layoutResId, List data, float maxNum, float lineMaxLength) {
+    public HorizontalChartAdapterSingle(int layoutResId, List data, float maxNum, float lineMaxLength) {
         super(layoutResId, data);
         this.maxNum = maxNum;
         this.lineMaxLength = lineMaxLength;
@@ -27,13 +25,11 @@ public class HorizontalChartAdapterDual extends BaseQuickAdapter<HorizontalChart
 
     @Override
     protected void convert(BaseViewHolder helper, HorizontalChartItem item) {
+        helper.setText(R.id.station, item.getZone_name());
         helper.setText(R.id.number, item.getNum() + "");
         helper.setText(R.id.rate, item.getRate());
-        helper.setText(R.id.station, item.getZone_name());
         LinearLayout lineNum = helper.getView(R.id.number_line);
         float numLength = (item.getNum() / maxNum) * lineMaxLength;
         lineNum.setMinimumWidth((int) numLength);
-        LinearLayout lineRate = helper.getView(R.id.rate_line);
-        lineRate.setMinimumWidth((int) ((Float.parseFloat(item.getRate()) / 100f) * numLength));
     }
 }
